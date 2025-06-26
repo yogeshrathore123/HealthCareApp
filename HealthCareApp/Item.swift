5591424197430b81745173f1d3c5d70a6b8107c4
Yogesh Rathore <yogeshrathore251@gmail.com>
2025-06-26 12:09:26 +0530

    Project Created

---
+++ HealthCareApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HealthCareApp
+//
+//  Created by Yogesh Rathore on 26/06/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
